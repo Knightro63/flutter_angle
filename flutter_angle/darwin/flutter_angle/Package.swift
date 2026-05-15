5f18cc25d22a6f9f619343bcf02280bb27a93d97
Knightro <50028636+Knightro63@users.noreply.github.com>
2026-05-15 16:06:19 -0400

    fixing issues

--- flutter_angle/darwin/flutter_angle/Package.swift
+++ flutter_angle/darwin/flutter_angle/Package.swift
@@ -16,13 +16,18 @@ let package = Package(
     targets: [
         .target(
             name: "flutter_angle",
-            dependencies: ["flutter_angle_binaries"],
+            dependencies: ["libEGL", "libGLESv2"],
             resources: [],
         ),
         .binaryTarget(
-            name: "flutter_angle_binaries",
-            url: "https://raw.githubusercontent.com/Knightro63/flutter_angle/refs/heads/main/flutter_angle_binaries.artifactbundle.zip",
-            checksum: "8e2d3480b4760babca355ebb10eb3ec0ea8d537c2bfd2bff268cbccbf6261bfd"
+            name: "libEGL",
+            url: "https://raw.githubusercontent.com/Knightro63/flutter_angle/refs/heads/main/libEGL.xcframework.zip",
+            checksum: "d02766406d39daae169465c235b3f98a3d036799d3f5155ec51b0a4437d18dae"
+        ),
+        .binaryTarget(
+            name: "libGLESv2",
+            url: "https://raw.githubusercontent.com/Knightro63/flutter_angle/refs/heads/main/libGLESv2.xcframework.zip",
+            checksum: "bc0b761a1e6797b2d5dced822cc76d4c119cd7f1bf342c6552d95418df730391"
         ),
     ]
 )
